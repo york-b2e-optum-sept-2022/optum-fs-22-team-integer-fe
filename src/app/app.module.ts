2c49913db9576c36f1f9f372c027059e5c4e6ef4
Cole <letchro@gmail.com>
2022-11-08 13:10:14 -0600

    Added init components and account, cart, and product services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { NavbarComponent } from './__navbar/navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FilterSidebarComponent } from './filter-sidebar/filter-sidebar.component';
+import { CustomerComponent } from './_customer/customer.component';
+import { ShopkeeperComponent } from './_shopkeeper/shopkeeper.component';
+import { AdminComponent } from './_admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductComponent,
+    NavbarComponent,
+    CartComponent,
+    RegisterComponent,
+    LoginComponent,
+    FilterSidebarComponent,
+    CustomerComponent,
+    ShopkeeperComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
