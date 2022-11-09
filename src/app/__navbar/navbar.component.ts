7c53cd49dd77dd321b69f7c32d6014d9f31ee238
Cole <letchro@gmail.com>
2022-11-09 16:29:30 -0600

    added rendering for logout and type views for different accounts

--- src/app/__navbar/navbar.component.ts
+++ src/app/__navbar/navbar.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {CartService} from "../cart.service";
 import {AccountService} from "../account.service";
+import {IAccount} from "../___interfaces/IAccount";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor(private cartService: CartService, private accountService: AccountService) { }
+  public account: IAccount | null = null;
 
-  ngOnInit(): void {
+  constructor(private cartService: CartService, private accountService: AccountService) {
+    this.accountService.$account.subscribe({
+      next: (account) => {
+          this.account = account
+
+      },
+      error: (err) => {
+        console.error(err);
+      }
+
+    })
   }
 
   onViewLogin() {
@@ -24,6 +35,10 @@ export class NavbarComponent implements OnInit {
 
   onCart() {
     this.cartService.viewCart();
+  }
+
+  onLogout() {
+    this.accountService.logout();
   }
 
 }
