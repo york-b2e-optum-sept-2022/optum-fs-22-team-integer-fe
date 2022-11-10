f71386bb7373b18ca1fca62adbd8d05a774d27fd
Cole <letchro@gmail.com>
2022-11-10 15:35:31 -0600

    working on rendering account views

--- src/app/_customer/customer.component.ts
+++ src/app/_customer/customer.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subscription} from "rxjs";
+import {AccountService} from "../account.service";
+import {IAccount} from "../___interfaces/IAccount";
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent implements OnInit {
+export class CustomerComponent implements OnDestroy{
 
-  constructor() { }
+  account!: IAccount;
 
-  ngOnInit(): void {
+  subscription: Subscription;
+
+  constructor(private accountService: AccountService) {
+    this.subscription = this.accountService.$account.subscribe({
+      next: (account) => {
+        if(account) {
+          this.account = account;
+        }
+      },
+      error: (err) => {
+        // TODO add error handling
+      }
+    })
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 
 }
