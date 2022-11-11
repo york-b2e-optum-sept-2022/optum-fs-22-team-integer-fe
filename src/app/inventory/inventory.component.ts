69dcb1d23d48f091e028de340328cc643c84ea83
Matt <mbauer1221@gmail.com>
2022-11-11 12:13:25 -0500

    added ability to search for product to update

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ViewService} from "../view.service";
+import {ProductService} from "../product.service";
+import {IProduct} from "../___interfaces/IProduct";
 
 @Component({
   selector: 'app-inventory',
@@ -8,13 +10,27 @@ import {ViewService} from "../view.service";
 })
 export class InventoryComponent implements OnInit {
 
-  constructor(private viewService: ViewService) { }
+  productList!: IProduct[]
+  selectedProduct!: IProduct | null
+
+  constructor(private viewService: ViewService, private productService: ProductService) {
+    this.productService.$productList.subscribe(
+      list => this.productList = list
+    )
+  }
 
   ngOnInit(): void {
   }
+
 
   onClose() {
     this.viewService.viewCloseInventory();
   }
 
+  onSelectProductClick(productId: string) {
+    for(let product of this.productList){
+      if(product.id === parseInt(productId))
+        this.selectedProduct = product
+    }
+  }
 }
