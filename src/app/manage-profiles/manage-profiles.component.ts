04ef6cdb4056d49c2ddb7624e7c1b14dff601f93
Cole <letchro@gmail.com>
2022-11-12 18:46:50 -0600

    added ability for admin to create new accounts and delete the cart that is connected to an account that is deleted

--- src/app/manage-profiles/manage-profiles.component.ts
+++ src/app/manage-profiles/manage-profiles.component.ts
@@ -15,22 +15,35 @@ export class ManageProfilesComponent implements OnInit, OnDestroy {
   selectedAccount!: IAccount | null
   errorMessage: string = "";
 
+  viewCreateAccount: boolean = false;
+  viewEditAccount: boolean = false;
+  createAccountEmail: string = "";
+  createAccountPassword: string = "";
+  createAccountType: number = 1;
+
   subscription: Subscription;
   subscription2: Subscription;
+  subscription3: Subscription;
 
   constructor(private accountService: AccountService, private viewService: ViewService) {
     this.subscription = this.accountService.$accountList.subscribe({
-        next: (accountList) => {
-          if (accountList) {
-            this.accountList = accountList
-          }
-        },
-        error: () => {}
+      next: (accountList) => {
+        if (accountList) {this.accountList = accountList}
+      },
+      error: () => {}
     });
-
     this.subscription2 = this.accountService.$accountError.subscribe((errorMessage) => {
       this.errorMessage = errorMessage;
     });
+    this.subscription3 = this.accountService.$registrationError.subscribe((errorMessage) => {
+      this.errorMessage = errorMessage;
+    });
+    this.viewService.$viewCreateAccount.subscribe(
+      (viewCreateAccount) => this.viewCreateAccount = viewCreateAccount
+    );
+    this.viewService.$viewEditAccount.subscribe(
+      (viewEditAccount) => this.viewEditAccount = viewEditAccount
+    );
   }
 
   ngOnInit(): void {
@@ -40,6 +53,7 @@ export class ManageProfilesComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
     this.subscription2.unsubscribe();
+    this.subscription3.unsubscribe();
   }
 
   onClose() {
@@ -52,11 +66,12 @@ export class ManageProfilesComponent implements OnInit, OnDestroy {
       if (account.id === parseInt(accountId))
         this.selectedAccount = account
     }
+    this.viewService.viewEditAccount();
   }
 
   onDeleteClick() {
     this.resetAccountError();
-    if(this.selectedAccount) {
+    if (this.selectedAccount) {
       this.accountService.deleteAccount(this.selectedAccount.id);
     }
   }
@@ -75,6 +90,29 @@ export class ManageProfilesComponent implements OnInit, OnDestroy {
 
   resetAccountError() {
     this.accountService.$accountError.next("");
+  }
+
+  onCreateAccountClick() {
+    this.resetAccountError();
+    this.viewService.viewCreateAccount();
+  }
+
+  onCancelCreateClick() {
+    this.viewCreateAccount = false
+    this.createAccountEmail = "";
+    this.createAccountPassword = "";
+  }
+
+  onSubmitNew() {
+    this.accountService.createAccount(
+      this.createAccountEmail,
+      this.createAccountPassword,
+      this.createAccountType
+    );
+  }
+
+  onCancel() {
+    this.selectedAccount = null;
   }
 
 }
