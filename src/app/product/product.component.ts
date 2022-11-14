29f409e3f3185a1bfaaa2f3b2610e382eb33a1fb
Jackson H <jackson.york.solutions@gmail.com>
2022-11-13 23:18:59 -0600

    Working on sale

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IProduct} from "../___interfaces/IProduct";
 import {CartService} from "../cart.service";
 
@@ -7,16 +7,32 @@ import {CartService} from "../cart.service";
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
   @Input() product!: IProduct;
+  isOnSale: boolean = false;
 
   constructor(private cartService: CartService) {
 
   }
 
+  ngOnInit() {
+    this.getIsOnSale()
+  }
+
   onClickAddProduct(){
     this.cartService.addProduct(this.product);
   }
+
+  getIsOnSale(){
+    this.isOnSale = false;
+    if (this.product.salePercentOff &&
+      new Date(this.product.saleStartDate).getDate() <= new Date().getDate() &&
+      new Date(this.product.saleEndDate).getDate() >= new Date().getDate()) {
+      this.isOnSale = true;
+    }
+  }
+
+
 
 }
